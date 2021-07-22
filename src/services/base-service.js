68f36e98d87c04d0fabea30203bfe194faad9329
Gabriel Bastos <gabriel.bastos@thoughtworks.com>
2021-07-22 15:27:54 -0300

    fix: bug in base-service GET request

--- src/services/base-service.js
+++ src/services/base-service.js
@@ -1,23 +1,25 @@
 const baseFetch = (route = "", body = "", method) => {
-    return fetch('https://carango-bom-api.herokuapp.com/' + route,
-    {
-        method,
-        body: JSON.stringify(body)
-    })
-    .then(r => r.json())
-}
+    let args = { method };
+
+    if (body !== null) {
+        args.body = JSON.stringify(body);
+    }
+
+    return fetch('https://carango-bom-api.herokuapp.com/' + route, args)
+        .then(r => r.json());
+};
 
 export const basePost = (route = "", body = "") => {
-    const method = "POST"
-    return baseFetch(route, body, method)
+    const method = "POST";
+    return baseFetch(route, body, method);
 }
 
-export const baseGet = (route = "", body = "") => {
-    const method = "GET"
-    return baseFetch(route, body, method)
+export const baseGet = (route = "") => {
+    const method = "GET";
+    return baseFetch(route, null, method);
 }
 
 export const baseDelete = (route = "", body = "") => {
-    const method = "DELETE"
-    return baseFetch(route, body, method)
+    const method = "DELETE";
+    return baseFetch(route, body, method);
 }
